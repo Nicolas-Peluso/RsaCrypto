372b5f294ad53d522904e2984edc1540b4d4f6dd
Nicolas <pelusonicolas7@gmail.com>
2023-01-19 11:21:41 -0300

    manual e encriptação/desencriptar em arquivos

--- main.py
+++ main.py
@@ -53,13 +53,15 @@ def EncriptaAsc2(menssagem):
         asc2Message.append(ascword);
     return asc2Message;
 
+
 def Painel():
     print("");
-    print("cripto -- RSA --");
+    print("#_#_#_#_#_#_#_#_RSA_#_#__#_##_#__##_");
     print("1-) Definir chaves");
     print("2-) criptografar ");
     print("3-) descriptografar ");
-    print("4-) sair");
+    print("4-) Manual")
+    print("5-) sair");
 
 
 def Painel_Criptografar():
@@ -69,6 +71,7 @@ def Painel_Criptografar():
     print("2-) criptografar uma menssagem em um arquivo: ");
     print("3-) sair");
 
+
 def Painel_Descriptografar():
     print("");
     print("cripto -- RSA --");
@@ -77,35 +80,86 @@ def Painel_Descriptografar():
     print("3-) sair");
 
 
+def man():
+    print("---------Manual---------");
+    print("Como usar função definir chaves");
+    print("1-) Defina dois numeros primos esses numeros não devem se compartilhados com ninguem quanto maior os primos mais seguro");
+    print("2-) defina N entre 1 e f(N) desde de que N não tenha um divisor comum com f(N) desconsiderando o 1");
+    print("3-) Caso o numero tenha algum divsor com f(N) você tera de escolher outro até achar um que satisfaça");
+    print("4-) Em caso de Erro 'Não há inverso multiplicativo modular para este número' voce dvera repetir o passo 2");
+    print("5-) Guarde as chaves privadas e as publicas a chaves publicas pode ser compartilada com qualquer um as privadas não");
+    print("");
+    print("Como usar função Criptografar");
+    print("1-) Escolha Se voce quer criptografar uma menssagem pequena no terminal ou um texto grande em um arquivo");
+    print("2-) Em ambos os casos Passe a chave Publica E e depois a chave publica N");
+    print("3-) Caso escolha a opção 1 digite a menssagem no terminal, e copie a messagem criptografada");
+    print("4-) Caso escolha a opção 2 Voce deve ter um arquivo chamado Entrada.txt na raiz do projeto esse arquivo deve conter o texto a ser criptografado");
+    print("5-) Após a criptografia ser realizada um arquivo chamado 'Final.txt' sera gerado o Arquivo 'Entrada.txt' com o texto original pode ser deletado");
+    print("");
+    print("Como usar função Descriptografar");
+    print("1-) Escolha Se voce quer Descriptografar uma menssagem pequena no terminal ou um texto grande em um arquivo");
+    print("2-) Em ambos os casos Passe a chave privada (NÂO OS NUMEROS PRIMOS ESCOLHIDOS) e depois a chave publica N");
+    print("3-) Caso escolha a opção 1 digite a Cole os blocos criptorgrafados no terminal, se tudo correr bem voce tera sua menssagem original");
+    print("4-) Caso escolha a opção 2 Voce deve ter um arquivo chamado Final.txt na raiz do projeto esse arquivo deve conter os blocos criptografados");
+    print("5-) Nesse mesmo arquivo Final.txt sera gerado o texto original");
+    print("LEMBRASSE QUE SE VOCE PERDER A CHAVE PRIVADA NÃO TEM COMO RECUPERAR O ARQUIVO ORIGINAL");
+    print("Se voce ja possui as chaves publicas e privadas voce não precisa criar novamente as chaves");
+    print("Esse prgrama foi desenvolvido por um estudante da computação não o use para criptografar arquivos senssiveis, use-o por sua conta e risco");
+
+
 def criptografar(chaveE, chaveN, opt):
-    Menssagem = str(input("Digite a menssagem a ser criptografada (lembresse que textos não podem ser colados, devem ser escritos no terminal): "));
-    if(len(Menssagem) == 0):
-        print("menssagem não pode estar vazia");
-        
-    EncriptaAsc2(Menssagem);
+    if(opt == 1):
+        Menssagem = str(input("Digite a menssagem a ser criptografada (lembresse que textos não podem ser colados, devem ser escritos no terminal): "));
+        if(len(Menssagem) == 0):
+            print("menssagem não pode estar vazia");
+
+    if(opt == 2):
+        File = open("Entrada.txt", "r");
+        text = File.read();
+        EncriptaAsc2(text);
+        File.close();
+
+    else:
+        EncriptaAsc2(Menssagem);
 
     for index in range(len(asc2Message)):
         C = asc2Message[index] ** chaveE % chaveN;
         encripetedMessage.append(C);
-    if(opt == 1):
-        print("Tudo certo copie sua menssagem abaixo e lembresse de salvar sua chave privada rsa. (os numeros primos não podem ser usados na descriptografia)");
-        print(encripetedMessage);
-    else:
+
+    if(opt == 2):
         ArqFinal = open("Final.txt", "w");
         for blocks in encripetedMessage:
-            ArqFinal.write(str(blocks))
-            ArqFinal.write("\n")
-        ArqFinal.close()
+            ArqFinal.write(str(blocks));
+            ArqFinal.write("\n");
+        ArqFinal.close();
+
+    print("Tudo certo copie sua menssagem abaixo e lembresse de salvar sua chave privada rsa. (os numeros primos não podem ser usados na descriptografia)");
+    print(encripetedMessage);
+   # else:
+     
 
 
 def descriptografar(Chave, modN, DescriOpt):
     FinalMessage = "";
-    for index in range(len(asc2Message)):
-        M = encripetedMessage[index] ** Chave % modN;
-        FinalMessage += chr(M);
-    
+
+    if(DescriOpt == 1):
+        for index in range(len(asc2Message)):
+            M = encripetedMessage[index] ** Chave % modN;
+            FinalMessage += chr(M);
+    else:
+        Arquivo = open('Final.txt', 'r');
+        lista = Arquivo.readlines();
+        Arquivo.close();
+
+        for line in lista:
+            M = int(line) ** Chave % modN;
+            FinalMessage += chr(M);
+        
+        Final = open("Final.txt", "w");
+        Final.write(FinalMessage);
+        Final.close();
+
     print("SUCESSO");
-    print(FinalMessage);
 
 
 def DefinirChaves():
@@ -153,7 +207,7 @@ def DefinirChaves():
 def main():
     while True:
         Painel();
-        opcao = int(input("digite uma das opções acima"));
+        opcao = int(input(">> "));
 
         match opcao:
             case 1:
@@ -167,14 +221,16 @@ def main():
                     criptografar(E, N, criptOpt);
             case 3:
                     Painel_Descriptografar();
-                    optDescri = int(input(">>"))
-                    Private1 = int(input("digite a chave privada: "))
-                    ModN = int(input("digite o a chave publica N (DEVE SER A CHAVE N): "))
+                    optDescri = int(input(">> "));
+                    Private1 = int(input("digite a chave privada: "));
+                    ModN = int(input("digite o a chave publica N (DEVE SER A CHAVE N): "));
 
-                    descriptografar(Private1, ModN, DecriptOpt)
+                    descriptografar(Private1, ModN, optDescri);
             case 4:
+                man()
+            case 5:
                 break;
             case _:
-                print("opção invalida");
+                print("opção invalida")
 
 main();
